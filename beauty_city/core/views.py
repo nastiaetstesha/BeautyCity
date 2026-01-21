a9bc0a9cfb4f274adf540e85b953b7f467165586
Azad0645 <fenix16450@gmail.com>
2026-01-22 00:43:48 +0300

    Подключил шаблоны к Django

--- beauty_city/core/views.py
+++ beauty_city/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def admin_page(request):
+    return render(request, 'admin.html')
+
+
+def notes(request):
+    return render(request, 'notes.html')
+
+
+def popup_examples(request):
+    return render(request, 'popup.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def service_finally(request):
+    return render(request, 'serviceFinally.html')
